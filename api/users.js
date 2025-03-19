a89ee8ae2dc96a9837b08ebf5bda6d010a507308
suhrob57 <teslakanal001@gmail.com>
2025-03-19 18:42:00 +0500

    Update users.js

--- api/users.js
+++ api/users.js
@@ -1,14 +1,5 @@
-import { Pool } from 'pg';
+const users = [];
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
-
-export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    const { user_id } = req.query;
-    const result = await pool.query('SELECT username FROM users WHERE user_id = $1', [user_id]);
-    res.status(200).json(result.rows[0]);
-  }
-}
+export const getUsers = (req, res) => {
+  res.json(users);
+};
